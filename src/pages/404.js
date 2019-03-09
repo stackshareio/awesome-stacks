107d7645a11f5b5f3c782490945dd282985002b7
Josh Dzielak <jdzielak@gmail.com>
2019-03-09 15:26:04 +0100

    Better 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,7 +8,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>There's no Awesome Stack here yet. <Link to="/">Create one?</Link></p>
   </Layout>
 )
 
