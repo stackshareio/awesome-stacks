45d5b2b87ff6bf066e821155ab5879ae81adba18
Josh Dzielak <jdzielak@gmail.com>
2019-03-12 01:48:43 +0100

    Fix the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,8 +7,21 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>There's no Awesome Stack here yet. <Link to="/">Create one?</Link></p>
+    <div className="section">
+      <div className="container">
+        <div className="columns is-centered">
+          <div className="column is-10">
+            <div className="message is-danger">
+              <div className="message-body">
+                <h1 className="is-size-3">404 AWESOMENESS NOT FOUND</h1>
+                <p className="has-margin-top-30 has-margin-bottom-30">There's no Awesome Stack here yet. <Link to="/">Create one?</Link></p>
+              </div>
+              <div className="has-margin-bottom-60"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
