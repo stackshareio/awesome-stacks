df135bd538ae0fe1b316ce3f2ee21553e94fe9de
dzello <jdzielak@gmail.com>
2019-03-14 02:26:18 +0100

    Support anchor navigation on docs page

--- src/pages/docs.js
+++ src/pages/docs.js
@@ -1,7 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Contributing from "../snippets/contributing"
+import Docs from "../snippets/docs"
+import { MDXProvider } from '@mdx-js/tag';
+
+const header = props => <div className="anchor"><a name={`${props.children.replace(/[\ -]/g, ``).toLowerCase()}`}></a><h2 className="" {...props} /></div>
+const components = {
+  h2: header
+}
 
 const ContributingPage = () => (
   <Layout>
@@ -11,17 +17,19 @@ const ContributingPage = () => (
         <div className="columns">
           <div className="column is-9">
             <div className="content">
-              <Contributing />
+              <MDXProvider components={components}>
+              <Docs />
+              </MDXProvider>
             </div>
           </div>
           <div className="column is-3">
-            <div class="box grey-box">
+            <div className="box grey-box is-fixed has-width-200">
               <h3 className="is-size-5 is-uppercase">Contents</h3>
               <ul>
                 <li><a href="#overview">&rsaquo; Overview</a></li>
                 <li><a href="#setup">&rsaquo; Setup</a></li>
-                <li><a href="#create-stack">&rsaquo; Create a stack</a></li>
-                <li><a href="#add-tools">&rsaquo; Add tools</a></li>
+                <li><a href="#createastack">&rsaquo; Create a stack</a></li>
+                <li><a href="#addtools">&rsaquo; Add tools</a></li>
                 <li><a href="#review">&rsaquo; Review</a></li>
               </ul>
             </div>
