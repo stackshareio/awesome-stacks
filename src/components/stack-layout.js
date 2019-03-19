1681e916d1149965392346726ebae1d4505fc15c
Josh Dzielak <jdzielak@gmail.com>
2019-03-19 19:20:37 +0100

    Show contributor avatars in stack header

--- src/components/stack-layout.js
+++ src/components/stack-layout.js
@@ -37,6 +37,13 @@ function StackLayout({ data }) {
                 <p className="is-size-5">{mdx.frontmatter.description}</p>
               </div>
               <div className="column is-4 has-text-right" style={{ marginTop: `auto` }}>
+                <div style={{ alignSelf: "flex-start" }}>
+                  {mdx.fields.contributors.slice(0, 4).map((contributor) => (
+                    <a key={contributor.login} href={contributor.url} style={{ display: "inline-block", margin: "2px" }}>
+                      <img style={{ width: "36px", height: "36px", borderRadius: "10px" }} src={contributor.avatarUrl} />
+                    </a>
+                  ))}
+                </div>
                 <ul>
                   <li>
                     Contributed by {mdx.fields.contributors.map(contributor => <a key={contributor.login} href={contributor.url} className="is-strong">@{contributor.login}</a>).map((item, index) => [index > 0 && ' ', item])}
