eb2875d48045e9f4f35c403c4e2abffffc5923dc
Josh Dzielak <jdzielak@gmail.com>
2019-04-10 20:56:17 +0200

    Improve PH banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -2,8 +2,7 @@ import React from "react"
 
 const Banner = () => (
   <div className="has-text-centered" style={{ width: "100%", padding: "10px 0", background: "#DA552F", zIndex: 999 }}>
-  <span role="img" aria-label="Cat emoji">😺</span> &nbsp;
-    <a className="has-text-white" href="https://www.producthunt.com/posts/awesome-stacks" style={{ textDecoration: "none" }}>See it on ProductHunt ↗</a>
+    <a rel="noopener noreferrer" href="https://www.producthunt.com/posts/awesome-stacks?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-awesome-stacks" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=150969&theme=light" alt="Awesome Stacks - Discover & share useful tech stacks for your next project 😎 | Product Hunt Embed" style={{ width: "225px", height: "54px" }} /></a>
   </div>
 )
 
